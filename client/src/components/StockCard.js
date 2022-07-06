65a729fd12538da9ba918ac68eed23d771981da4
Doug Sellers <DouglasSSellers@gmail.com>
2022-07-05 22:40:03 -0400

    added new StockCard component

---
+++ client/src/components/StockCard.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const StockCard = () => {
+  return (
+    <div>
+      <h1>Stock Symbol</h1>
+      <h2>Stock Value</h2>
+      <h4>Stock Info</h4>
+    </div>
+  )
+}
+
+export default StockCard
