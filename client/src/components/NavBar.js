dfa503522379817c049700b3e1587df02592b714
Doug Sellers <DouglasSSellers@gmail.com>
2022-07-19 23:08:51 -0400

    rendered stock clicked

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from 'react-router-dom'
 import logo from '../images/stockLogo.jpeg'
 
 class NavBar extends React.Component {
@@ -8,7 +7,6 @@ class NavBar extends React.Component {
     return (
       <div>
         <img alt={logo} src={logo} />
-        <Link to="/home">Home</Link>
       </div>
     )
   }
