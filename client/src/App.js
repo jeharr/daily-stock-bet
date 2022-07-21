b28ed3b2d2c0d49a73b54754244059f13b9d205c
Doug Sellers <DouglasSSellers@gmail.com>
2022-07-21 15:53:19 -0400

    added ability to choose specific stock ticker

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,16 @@
 import React from 'react'
 import { Router, Route } from 'react-router-dom'
-import axios from 'axios'
 //import { customHistory } from './stores'
 
 import StockPicker from './components/StockPicker'
+import NavBar from './components/NavBar'
 
 function App() {
 
-  const getData = () => {
-    console.log("GETTING DATA")
-    axios({
-      method: 'get',
-      url: 'https://yfapi.net/v1/finance/trending/US',
-      headers: {
-        'X-API-KEY': 'HoQHKV0tbk9a436WLsg1K2N7bPAFbVRq6pvgQA5t'
-      }
-    }).then((res) => {
-      console.log("FROM YAHOO", res)
-    })
-  }
+
   return (
     <div className="App">
-      <button onClick={getData}>click me</button>
+      <NavBar />
       <StockPicker />
     </div>
   );
