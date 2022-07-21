2966a1df25134028fac73f6bc1c6e53c9b5fdb2c
Doug Sellers <DouglasSSellers@gmail.com>
2022-07-21 10:58:54 -0400

    added StockPicker component

--- client/src/components/StockPicker.js
+++ client/src/components/StockPicker.js
@@ -1,35 +1,44 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 import styles from './StockPicker.scss'
 
 const StockPicker = () => {
 
-  const [selectedSymbol, setSelectedSymbol] = useState('')
+  const [stockTicker, setStockTicker] = useState([])
+
+  const getStockTicker = () => {
+    axios({
+      method: 'get',
+      url: 'https://yfapi.net/v1/finance/trending/US',
+      headers: {
+        'X-API-KEY': 'HoQHKV0tbk9a436WLsg1K2N7bPAFbVRq6pvgQA5t'
+      }
+    }).then((res) => {
+      console.log('FROM YAHOO API', res)
+
+      setStockTicker(res.data.finance.result[0].quotes)
+      console.log(stockTicker)
+    }).catch((err) => {
+      console.log('THIS IS THE ERROR: ', err)
+    })
+  }
+
+  useEffect(() => {
+    getStockTicker()
+  }, [])
+
 
   return (
     <div className={styles.stockPickerContainer}>
       this is the container for all the stocks
-      <div className={styles.stock}>
-          <div>
-            stock name one
-          </div>
-      </div>
-      <div className={styles.stock}>
-          <div>
-            stock name two
-          </div>
-      </div>
-      <div className={styles.stock}>
-          <div>
-            stock name three
-          </div>
-      </div>
-      <div className={styles.stock}>
-          <div>
-            stock name four
-          </div>
-      </div>
+
+      {stockTicker.map((ticker) => {
+        return (
+          <li key={ticker.symbol}>{ticker.symbol}</li>
+        )
+      })}
+
     </div>
   )
 }
