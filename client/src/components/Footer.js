c028e37dc9abb447750fd75debe18fb0b013e008
Doug Sellers <DouglasSSellers@gmail.com>
2022-07-26 20:07:14 -0400

    added Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <dev>
+      <dev>contact information</dev>
+      <a href='https://finance.yahoo.com/'>Yahoo</a>
+    </dev>
+  )
+}
+
+export default Footer
